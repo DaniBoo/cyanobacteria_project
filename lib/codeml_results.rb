d02cc3e80baf2c7443beb62df7293ec6ffc69806
Danielle Boobyer <dani.boobyer@gmail.com>
2013-01-26 17:29:59 +0000

    Got the class pulling out a couple of values from the raw file (lnL and tree length)
    Also added in empty float and string classes in case I need to extend
    them. May delete later.

--- lib/codeml_results.rb
+++ lib/codeml_results.rb
@@ -1,8 +1,36 @@
 class CodemlResults
 
+  attr_reader :raw_output, :tree_length, :lnL
+
   # Reads in a file
   def read(filename)
     buffer = File.read filename if File.exist? filename
   end
 
+  # Gives the raw output of the results
+  def raw_output
+    self.read('codeml_files/mlc')
+  end
+
+  # Extracts the tree
+
+  def tree_length
+    # find the tree length in the raw output using a regex
+    raw_output[/tree[\s]+length[\s]+=[\s]+(\d+).(\d+)/][/(\d+).(\d+)/]
+  end
+
+  # Extracts the lnL
+  def lnL()
+    # find the lnL in the raw output using a regex
+    # works on the assumption it's the only negative number in the file
+    raw_output[/-(\d+)*.(\d+)/]
+  end
+
 end
+
+my_results = CodemlResults.new
+
+puts "Tree length:"
+puts my_results.tree_length
+puts "\nlnL:"
+puts my_results.lnL
--- spec/codeml_results_spec.rb
+++ spec/codeml_results_spec.rb
@@ -1,21 +1,35 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe CodemlResults do
 
   let(:results) {CodemlResults.new}
   
-  context ":opening a file:" do
+  # The tests for the read() method
+  context "read() a file" do
 
     it "fails when there's no file" do
       # Safe to assume there's never going to be a file called 'no_such_file'
-      results.read('no_such_file').should_not be_nil
+      # So, if we try to read in 'no_such_file' we're expecting it to return nil
+      # as the file doesn't exist
+      results.read("no_such_file").should be_nil
     end
     
     it "succeeds when there is a file" do
       # Safe to assume there's going to be a file called 'codeml_results.rb'
-      results.read('spec/spec_helper.rb').should be_nil
+      # So, if we try to read in 'codeml_results.rb' we're expecting it NOT to return nil
+      # as the file DOES exist
+      results.read("spec/spec_helper.rb").should_not be_nil
     end
 
   end
+
+  # # The tests for the lnL() method
+  # context "get the lnL" do
+
+  #   it "fails when there's no occurence of the lnL in the " do
+  #     # 
+  #   end
+
+  # end
 
 end
