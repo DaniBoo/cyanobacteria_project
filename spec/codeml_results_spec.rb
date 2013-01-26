0bbb82dfc18762d96688ae7337b0385c5afb65be
Danielle Boobyer <dani.boobyer@gmail.com>
2013-01-26 16:05:08 +0000

    added a context to the spec for CodemlResults

--- spec/codeml_results_spec.rb
+++ spec/codeml_results_spec.rb
@@ -4,14 +4,18 @@ describe CodemlResults do
 
   let(:results) {CodemlResults.new}
   
-  it "fails when there's no file" do
-    # Safe to assume there's never going to be a file called 'no_such_file'
-    results.read('no_such_file').should be_nil
-  end
-  
-  it "succeeds when there is a file" do
-    # Safe to assume there's going to be a file called 'codeml_results.rb'
-    results.read('spec/spec_helper.rb').should_not be_nil
+  context ":opening a file:" do
+
+    it "fails when there's no file" do
+      # Safe to assume there's never going to be a file called 'no_such_file'
+      results.read('no_such_file').should_not be_nil
+    end
+    
+    it "succeeds when there is a file" do
+      # Safe to assume there's going to be a file called 'codeml_results.rb'
+      results.read('spec/spec_helper.rb').should be_nil
+    end
+
   end
 
 end
