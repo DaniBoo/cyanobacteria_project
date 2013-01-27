98634ded633e909a382074b4e03d1338d7bcb569
Danielle Boobyer <dani.boobyer@gmail.com>
2013-01-27 17:39:49 +0000

    Tidying up the cyan.rb file

--- cyan.rb
+++ cyan.rb
@@ -25,10 +25,31 @@ end.parse!
 # Directory
 options[:directory] ? directory = options[:directory] : directory = "codeml_files"
 
+# Control File
+options[:generate] ? control_filename = options[:generate] : control_filename = "codeml.ctl"
+
 
 # Generate the control file
 if options[:g]
   puts options[:generate]
+
+  # Create a new control file in memory
+  my_control_file = ControlFile.new
+
+  # Set whatever attributes we need to
+  # Model - if no F then is 2, if F then is 3
+  my_control_file.model = 2
+  # aaRatefile - depends on first part of model eg LG
+  my_control_file.aaRatefile = 'LG.dat'
+  # Fix_alpha - depends on model 0 unless no I and no G
+  my_control_file.fix_alpha = 0
+  # alpha - if no I, no G then is 0, else is 0.5
+  my_control_file.alpha
+  # alpha - if no I, no G then is 0. If I, no G is 2. If I and G is 5. If G, no I is 4.
+  my_control_file.nCatG
+
+  #Create the control file
+  puts my_control_file.create("#{directory}/#{control_filename}")
 end
 
 
@@ -40,7 +61,9 @@ if options[:parse]
   # Create a new CodemlResults object, based on a specified output file
   my_results = CodemlResults.new("#{directory}/#{filename}")
 
-  # Return the np/lnL/AIC
+  # TO DO: We'll need to export the data to some kind of file
+
+  # Return the np/lnL/AIC to the command line
   puts "np:"
   puts my_results.np
   puts "lnL:"
@@ -48,35 +71,3 @@ if options[:parse]
   puts "AIC:"
   puts my_results.AIC
 end
-
-
-
-# 
-# 
-
-# # Access the attributes as follows
-# puts "np:"
-# puts my_results.np
-# puts "lnL:"
-# puts my_results.lnL
-# puts "AIC:"
-# puts my_results.AIC
-
-# # Create a new control file in memory
-# my_control_file = ControlFile.new
-
-# # Set whatever attributes we need to
-# # Model - if no F then is 2, if F then is 3
-# my_control_file.model = 2
-# # aaRatefile - depends on first part of model eg LG
-# my_control_file.aaRatefile = 'LG.dat'
-# # Fix_alpha - depends on model 0 unless no I and no G
-# my_control_file.fix_alpha = 0
-# # alpha - if no I, no G then is 0, else is 0.5
-# my_control_file.alpha
-# # alpha - if no I, no G then is 0. If I, no G is 2. If I and G is 5. If G, no I is 4.
-# my_control_file.nCatG
-
-
-# #Create the control file
-# puts my_control_file.create('codeml1.ctl')
